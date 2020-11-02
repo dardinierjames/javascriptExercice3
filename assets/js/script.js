f2ebe74ca98bb10a00cc9972796bfd904ee5574b
dardinierjames <dardinier.james@orange.fr>
2020-11-02 17:17:24 +0100

    boiteDeDialogue

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function saisir_firstName(){
+
+    var firstName = prompt('Quel est votre nom ?')
+    // prompt ouvre une boite de dialogue
+        alert('Bonjour ' + firstName)
+        // alert également
+
+}
